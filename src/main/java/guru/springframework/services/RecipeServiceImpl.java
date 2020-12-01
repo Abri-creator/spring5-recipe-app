5d31dbe0d408b71c9319c98ba899b50c583d4e7f
Abrì-creator <zullogabriele2@gmai.com>
2020-12-01 15:05:11 +0100

    example of project lombok

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,10 +2,13 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -17,6 +20,9 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        //log property get declared. And project lombok  goes through and refactors the compiling for us
+        log.debug("I'm in the services");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
